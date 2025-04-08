9248291d8f0dfa20de606fc6bc0627b691184cc5
Davis-Patterson <Davisfpatterson@gmail.com>
2025-04-08 00:53:13 -0400

    updated patch notes

--- src/Components/Utils/PatchNotes.tsx
+++ src/Components/Utils/PatchNotes.tsx
@@ -1,4 +1,4 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { AppContext } from 'Contexts/AppContext';
 import PatchNotesData from 'Utilities/PatchNotes.json';
 import 'Styles/Utils/PatchNotes.css';
@@ -12,26 +12,51 @@ function PatchNotes() {
   const sortedNotes = [...PatchNotesData].sort(
     (a, b) => new Date(b.date).getTime() - new Date(a.date).getTime()
   );
+  const [selectedDate, setSelectedDate] = useState<string>(
+    sortedNotes[0]?.date || 'all'
+  );
+
+  const handleDateChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setSelectedDate(event.target.value);
+  };
+
+  const filteredNotes =
+    selectedDate === 'all'
+      ? sortedNotes
+      : sortedNotes.filter((note) => note.date === selectedDate);
 
   return (
     <div className='patch-notes-container'>
-      {sortedNotes.map((release) => (
+      <div className='patch-notes-header'>
+        <select
+          className='patch-date-selector'
+          value={selectedDate}
+          onChange={handleDateChange}
+        >
+          <option value='all'>All Updates</option>
+          {sortedNotes.map((release) => (
+            <option key={release.date} value={release.date}>
+              {release.date}
+            </option>
+          ))}
+        </select>
+      </div>
+
+      {filteredNotes.map((release) => (
         <div key={release.date} className='patch-date-card'>
           <h3 className='patch-date'>{release.date}</h3>
           <div className='patch-notes-list'>
             {release.patch_notes.map((note, index) => (
-              <>
-                <div className='patch-note-container' key={index}>
-                  <h4 className='note-header'>{note.header}</h4>
-                  <ul className='note-body'>
-                    {note.body.map((point, idx) => (
-                      <li key={idx} className='note-point'>
-                        <p className='note-body-text'>{point}</p>
-                      </li>
-                    ))}
-                  </ul>
-                </div>
-              </>
+              <div className='patch-note-container' key={index}>
+                <h4 className='note-header'>{note.header}</h4>
+                <ul className='note-body'>
+                  {note.body.map((point, idx) => (
+                    <li key={idx} className='note-point'>
+                      <p className='note-body-text'>{point}</p>
+                    </li>
+                  ))}
+                </ul>
+              </div>
             ))}
           </div>
         </div>
